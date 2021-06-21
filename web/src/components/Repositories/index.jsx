8cd84a091438438fdccdc3aa1e1919ade4220dc2
Josyane T <josyanestartarini@gmail.com>
2021-06-20 21:44:24 -0300

    <Repositories />
    List of repositories

---
+++ web/src/components/Repositories/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import RepoCard from '../RepoCard';
+import styles from './styles/Index.module.scss';
+
+const Repositories = ({ repos }) => {
+  console.log(repos);
+  return (
+
+    <article className={styles.container}>
+      {repos.map((repo) => (
+        <RepoCard
+          title={repo.name}
+          desc={repo.description}
+          star={repo.stargazers_count}
+          key={repo.id}
+        />
+      ))}
+    </article>
+
+  );
+};
+Repositories.propTypes = {
+  repos: PropTypes.array,
+};
+
+Repositories.defaultProps = {
+  repos: '',
+};
+
+export default Repositories;
