269e8ed6ac6ba18a74c72e303533f104e5377283
Josyane T <josyanestartarini@gmail.com>
2021-06-20 21:42:05 -0300

    <Link /> Next link

---
+++ web/src/components/Link/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NextLink from 'next/link';
+import styles from './style/Index.module.scss';
+
+export default function Link({ href, children }) {
+  return (
+    <NextLink href={href} passHref>
+      <a href className={styles.button}>{children}</a>
+    </NextLink>
+  );
+}
+
+Link.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
